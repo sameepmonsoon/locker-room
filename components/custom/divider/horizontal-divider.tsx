b3b472855e2f72a6ae989afa09125a20a45e58e1
sameep <rana.samip@webexpertsnepal.com>
2025-06-02 17:23:50 +0545

    utils : files created for APIs

--- components/custom/divider/horizontal-divider.tsx
+++ components/custom/divider/horizontal-divider.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import React from "react";
 import { Divider } from "@/components/ui/divider";
 import { Box } from "@/components/ui/box";
